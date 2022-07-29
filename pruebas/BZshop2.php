14e77cf1aac6689c543ce63f89c7c4fc904586a2
miguelcastrooo <109358986+miguelcastrooo@users.noreply.github.com>
2022-07-29 14:52:58 -0500

    mas actualizacion

--- pruebas/BZshop2.php
+++ pruebas/BZshop2.php
@@ -4,11 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
-=======
-    <link rel="stylesheet" href="../bootstrap/bootstrap.min.css" type="text/css">
->>>>>>> refs/remotes/origin/main
     <title>Document</title>
     
     <style>
@@ -23,12 +19,8 @@
 <body>
     <div>
         <?php 
-<<<<<<< HEAD
 		include ('nav.html')
 		
-=======
-		include ('navbienvenida.html')
->>>>>>> refs/remotes/origin/main
 		?>
     </div>
 	<br>
@@ -44,33 +36,21 @@
 	</div>
 	<div class="carousel-inner">
 	  <div class="carousel-item active">
-<<<<<<< HEAD
 		<img src="../imagenes/LogoBZ.jpeg" class="style="width: 50% >
-=======
-		<img src="../imagenes/LogoBZ.jpeg"" class="style="width: 50% >
->>>>>>> refs/remotes/origin/main
 		<div class="carousel-caption d-none d-md-block">
 		  <h5>esto es BZshop</h5>
 		  <p>somos la mejor tienda de ropa</p>
 		</div>
 	  </div>
 	  <div class="carousel-item">
-<<<<<<< HEAD
 		<img src="../imagenes/LogoBZ.jpeg" class="style="width: 100% >
-=======
-		<img src="../imagenes/52.png" class="style="width: 100% >
->>>>>>> refs/remotes/origin/main
 		<div class="carousel-caption d-none d-md-block">
 		  <h5>promociones</h5>
 		  <p>ven con nosotros a comprar</p>
 		</div>
 	  </div>
 	  <div class="carousel-item">
-<<<<<<< HEAD
 		<img src="../imagenes/LogoBZ.jpeg" class="d-block w-100" alt="...">
-=======
-		<img src="../imagenes/clothes.jpg" class="d-block w-100" alt="...">
->>>>>>> refs/remotes/origin/main
 		<div class="carousel-caption d-none d-md-block">
 		  <h5>moda de invierno y negro</h5>
 		  <p>la mejoores ofertas</p>
@@ -88,10 +68,6 @@
 
   </div>
   </center> 
-<<<<<<< HEAD
   <script src="../BZshop/js/bootstrap.min.js"></script>
-=======
-  <script src="../bootstrapjs/bootstrap.min.js"></script>
->>>>>>> refs/remotes/origin/main
 </body>
 </html>
