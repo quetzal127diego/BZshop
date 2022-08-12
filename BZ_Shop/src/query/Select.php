89cf6f0de4c6ecf66fd548758e99d5fdb3d877fb
DiegoIvC <109551862+DiegoIvC@users.noreply.github.com>
2022-08-12 10:10:59 -0500

    Actualizacion de diseño

---
+++ BZ_Shop/src/query/Select.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MyApp\Query;
+
+use PDO;
+use PDOException;
+use MyApp\Data\Database;
+
+class Select
+{
+    public function seleccionar($qry)
+    {
+        try
+        {
+            $cc=new Database("bzshop","admin","administrador");
+            $objetoPDO=$cc->getPDO();
+            $resultado=$objetoPDO->query($qry);
+            $fila=$resultado->fetchAll(PDO::FETCH_OBJ);
+            $cc->desconectarDB();
+            return $fila;
+        }
+        catch(PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+    }
+}
+?>
